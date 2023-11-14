db83466af3690339b7e7cf3e90960eef4d63f684
zsp <260435242@qq.com>
2023-11-14 23:56:44 +0800

    添加计价规则

--- internal-common/src/main/java/com/zsp/dto/PriceRule.java
+++ internal-common/src/main/java/com/zsp/dto/PriceRule.java
@@ -2,17 +2,59 @@ package com.zsp.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author 张世平
+ * @since 2023-11-14
+ */
 @Data
-public class PriceRule {
+public class PriceRule implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 城市编码
+     */
     private String cityCode;
 
+    /**
+     * 车辆类型
+     */
     private String vehicleType;
 
+    /**
+     * 起步价格
+     */
     private Double startFare;
 
+    /**
+     * 起步公里数
+     */
     private Integer startMile;
 
+    /**
+     * 计程单价
+     */
     private Double unitPricePerMile;
 
+    /**
+     * 计时单价
+     */
     private Double unitPricePerMinute;
+
+    /**
+     * 运价版本
+     */
+    private Integer fareVersion;
+
+    /**
+     * 运价类型
+     */
+    private String fareType;
+
 }
